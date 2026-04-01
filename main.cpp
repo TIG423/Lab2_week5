990fed9fa35d20469d65a95a116aaaf2d2b0c4eb
CSH <phantom7710@snu.ac.kr>
2026-04-01 17:11:35 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -6,17 +6,11 @@ using namespace std;
 int N,M;
 vector<int> vec;
 
-void func(int cnt) {
-
-
-
-
-
-
-
+void func(int cnt1,int cnt2) {
+   cout << cnt1+cnt2;
 }
 
 int main() {
    cin >> N >> M;
-   func(0);
+   func(N,M);
 }
